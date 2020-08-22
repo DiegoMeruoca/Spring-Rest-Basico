acc601edc6a5118abc0e48d969e002b6da68a8d2
DiegoMeruoca <diegomeruoca@gmail.com>
2020-08-22 01:46:03 -0300

    Criando a tabela Clientes com Flyway e alterando o model cliente para se conectar o DB com Jakarta Persistece utilizando Hibernate para realizar o Mapeamento Objeto Relacional

--- src/main/java/com/diegomeruoca/osteste/api/controller/ClienteController.java
+++ src/main/java/com/diegomeruoca/osteste/api/controller/ClienteController.java
@@ -1,31 +1,75 @@
 package com.diegomeruoca.osteste.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diegomeruoca.osteste.domain.model.Cliente;
+import com.diegomeruoca.osteste.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes") //Define a URL que o controlador responde
 public class ClienteController {
-	@GetMapping("/clientes")
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping //Executado ao dar Request GET n URL padrão
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		var cliente2 = new Cliente();
-		
-		cliente1.setId(1L);//Necessita do L por se tratar de long
-		cliente1.setNome("João Pedro Mendes");
-		cliente1.setEmail("jp.mendes@gmail.com");
-		cliente1.setTelefone("(11) 4615-4548");
-		
-		cliente2.setId(2L);//Necessita do L por se tratar de long
-		cliente2.setNome("Marina Pereira");
-		cliente2.setEmail("marina.pereira@gmail.com");
-		cliente2.setTelefone("(11) 4518-7518");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
-
+	
+	@GetMapping("/{clienteId}") //Executado ao dar Request GET na URL padrão + /{id}
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId); 
+		//Optional é utilizado pois a variavel pode ou não receber um cliente após a busca
+		
+		if (cliente.isPresent()) {//Caso encontre o cliente
+			return ResponseEntity.ok(cliente.get());//Retorna a resposta HTTP ok (200) e o cliente
+		}
+		
+		return ResponseEntity.notFound().build(); //Caso contrário responde não encontrado (404)
+	}
+	
+	@PostMapping //Executado ao dar Request POST na URL padrão
+	@ResponseStatus(HttpStatus.CREATED)//Pra responder o status 201 - Created
+	public Cliente adicionar(@RequestBody Cliente cliente) {//@RequestBody transforma o json vondo no body em um obj Java
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}") //Executado ao dar Request PUT na URL padrão + /{id}
+	public ResponseEntity<Cliente> batualizar(@PathVariable Long clienteId,
+			@RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);//Adiciona o is no obj
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}") //Executado ao dar Request DELETE na URL padrão + /{id}
+	public ResponseEntity<Void> excluir(@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(clienteId);
+		return ResponseEntity.noContent().build();//Retorna noContent (204)
+	}
+	
 }
--- src/main/java/com/diegomeruoca/osteste/domain/model/Cliente.java
+++ src/main/java/com/diegomeruoca/osteste/domain/model/Cliente.java
@@ -1,8 +1,24 @@
 package com.diegomeruoca.osteste.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/*Vamos usar a especificação Jakarta persistence para que possamos utilizar a implementação
+Hibernate para realizar o Mapeamento Objeto Relacional*/
+
+@Entity 
 public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Configura para gerar o Id automaticamente da forma nativa do BD utilizado
 	private Long id;
-	private String nome, email, telefone;
+	private String nome;
+	private String email;
+	
+	@Column(name = "fone")
+	private String telefone;
+	
 	public Long getId() {
 		return id;
 	}
@@ -27,6 +43,29 @@ public class Cliente {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
+	//Criação do hashCode e equals, para a tabela de Disperção
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
